e4e3252898ab6aaedfc82791cedd2f2447c6b812
YS AN <as5948@soongsil.ac.kr>
2021-01-21 20:15:50 +0900

    JS study 6.7 - 6.11

--- function.js
+++ function.js
@@ -292,3 +292,165 @@ greet = null;
 welcome(); // Hello, Guest (nested call works)
 welcome("AYS"); // Hello, AYS
 //*/
+
+/*
+//new Function은 모든 문자열을 함수로 바꿀 수 있음 -> 런타임에 문자가 그대로 전달 됨
+//  -> 서버에서 코드를 수신하거나 복잡한 웹 애플리케이션에서 템플릿에서 함수를 동적으로 컴파일하는 것과 같은 매우 특정한 경우에 사용
+let sum = new Function('a', 'b', 'return a + b');
+alert(sum(1, 2)) //3
+
+let Hello = new Function ('alert("HELLO WORLD")');
+Hello();
+
+function testNewFunc() {
+   let value = "test";
+
+   //let func = new Function('alert(value)'); //오류 - new function은 외부 변수 접근할 수 없음
+   let func = function() { alert(value); }; //test
+
+   return func;
+}
+testNewFunc()();
+//*/
+
+/*
+function sayHi(who) { alert(`${who}! Hello`); }
+//5초뒤에 sayHi 함수 한번 실행
+setTimeout(sayHi, 5000, 'AYS'); // 인스를 사용하려면 함수명, 초 간격, 인수 순서로 사용 + 함수명뒤에 괄호 붙이면 안돼! (syaHi()는 오류)
+let timer = setTimeout(() => alert('Hello'), 5000); //화살표 함수로 표현
+//setTimeout은 타이머 식별자를 반환함
+
+alert(`취소 전 : ${timer}`);
+clearTimeout(timer);  //함수 취소 -> 결과적으로는 아무 일도 일어나지 않음 (전 : 2 / 후 : 2)
+alert(`취소 후 : ${timer}`);
+//*/
+
+/*
+let intervalTimer = setInterval(() => alert('tick'), 2000); //2초마다 실행
+setTimeout(() => { clearInterval(intervalTimer); alert('stop'); }, 11000); //11초 뒤 타이머 중지
+
+//중첩 된 setTimeout
+// : 일정시간마다 타임 설정해서 delay초마다 반복하는 효과를 줌 (일정 또는 주기의 변화 줄 수 있음)
+let delay = 2000
+let timerId = setTimeout(function tick() {
+    alert(`[ ${delay} ]
+    tick`);
+    timerId = setTimeout(tick, delay += 2000); // (*)
+}, delay);
+//setInterval와 다르게 setTimeout를 반복해서 생성해주는거라 생성 텀이 더 생김 (더 오래 걸려)
+//*/
+
+
+/*
+let user = {
+    firstName: "AN",
+
+    sayHi() {
+        alert(`Hello, ${this.firstName}!`);
+    },
+
+    sayCustomer(say) {
+        alert(`${this.firstName} : "${say}"`);
+    },
+
+    sayCustomerTime(time, say) {
+        alert(`[${time}] ${this.firstName} : "${say}"`);
+    },
+
+    login(result) {
+        alert( this.firstName + (result ? ' logged in' : ' failed to log in') );
+    }
+};
+//setTimeout(user.sayHi, 1000); // Hello, undefined! - 외부 변수 접근 못하기 때문
+
+// 오류 - lost user context
+//let funcSayHi = user.sayHi;
+//setTimeout(funcSayHi, 1000);
+
+
+//1.wrapper
+setTimeout(function() { user.sayHi(); }, 1000); //function으로 wrapping해줌으로 해결
+setTimeout(() => user.sayHi(), 1000); // 위와 같은 맥락으로 해결 가능
+
+
+//2.bind
+function func_bind01() { alert(this.firstName); }
+function func_bind02(customer) { alert(customer + ', ' +this.firstName); }
+let funcUser01 = func_bind01.bind(user);
+let funcUser02 = func_bind02.bind(user);
+let funcUser03 = user.sayHi.bind(user);
+funcUser01(); // AN
+funcUser02(); //undefined, AN
+funcUser02("Hola"); //Hola, AN
+funcUser03(); //Hello, AN
+setTimeout(funcUser03, 3000); // 3초 후 실행
+
+let cust = user.sayCustomer.bind(user);
+cust("WOW"); //AN : "WOW"
+cust("LOL"); //AN : "LOL"
+
+
+//3.Partial functions (부분 함수)
+function mul(a, b) { return a * b; }
+let double = mul.bind(null, 2);
+let triple  = mul.bind(null, 3);
+
+alert(double(3)); // 6(2X3)
+alert(double(4)); // 8 (2X4)
+alert(triple(5)); // 15 (3X5)
+
+function partial(func, ...argsBound) {
+    return function(...args) {
+      return func.call(this, ...argsBound, ...args);
+    }
+}
+user.sayNow = partial(user.sayCustomerTime, new Date().getHours() + ':' + new Date().getMinutes());
+user.sayNow("Hello"); //[20:34] AN : "Hello"
+
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "q_981105") ok();
+    else fail();
+}
+
+askPassword(() => user.login(true), () => user.login(false));
+//*/
+
+let group = {
+    title: "Our Group",
+    students: ["John", "Pete", "Alice"],
+
+    //화살표 함수는 this의 개념이 없음
+    showList() {
+        this.students.forEach(
+            student => alert(this.title + ': ' + student) //Our Group: 이름-> this지만 외부 변수 접근
+        );
+
+        this.students.forEach(function(student) {
+            alert(this.title + ': ' + student) //undefined: 이름-> 내부에title가 없기 때문에 undefined 나옴
+        });
+    }
+};
+group.showList();
+
+
+function defer_A(f, ms) {
+    return function() {
+        setTimeout(() => f.apply(this, arguments), ms)
+    };
+}
+function defer_N(f, ms) {
+    return function(...args) {
+        let ctx = this;
+        setTimeout(function() { return f.apply(ctx, args);}, ms);
+    };
+}
+
+function sayHi(who) { alert('Hello, ' + who); }
+
+let sayHiDeferred = defer_A(sayHi, 2000);
+sayHiDeferred("John (A)"); // Hello, John after 2 seconds
+
+sayHiDeferred = defer_N(sayHi, 2000);
+sayHiDeferred("John (N)"); // Hello, John after 2 seconds
